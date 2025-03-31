55d05d9b4ef4849c044e2931a3b8b354986f7161
Mharia <ms5132425@gmail.com>
2025-03-31 19:51:14 -0300

    Exercicio 1

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-console.log("Mharia")
-console.log(20)
-console.warn("atenção")
-console.error("um erro")
-
-let nome = "Mharia"
-let idade = 38
-console.log(nome, idade)
-nome = "Mharia"
-console.log(nome)
-
 let nomeCompleto = ""
 nomeCompleto = prompt("Informe seu nome completo")
+
 console.log(nomeCompleto)
 alert("Seja bem vindo(a) " + nomeCompleto)
 document.write("Seja bem vindo(a)" + nomeCompleto)
 
-let cor = prompt("Informe uma cor \n 1 azul \n 2 vermelho \n 3 preto \n 4 rosa \n 5 verde \n 6 laranja")
+function fnTrocarCorDeFundo(){
+let cor = prompt("Informe uma cor \n 1 azul \n 2 vermelho \n 3 preto \n 4 rosa \n 5 verde \n 6 branco")
 let corDeFundo = null
 if(cor == "1"){
     corDeFundo = "blue"
@@ -33,12 +24,11 @@ else if(cor == "5"){
     corDeFundo = "green"
 }
 else if(cor == "6"){
-    corDeFundo = "orange"
+    corDeFundo = "white"
+}
+
+document.body.style.backgroundColor = corDeFundo
 }
 
 
-
-
-document.body.style.backgroundColor = corDeFundo
-
-
+fnTrocarCorDeFundo()
---
+++ exer-01/exer01.app.js
@@ -0,0 +1,6 @@
+let numero1 = prompt ("Informe o primeiro numero" )
+let numero2 = prompt("Informe o segundo numero ")
+
+let resultado = parseFloat (numero1) + parseFloat(numero2)
+
+alert("O resultado é " + resultado )
