b00e7482b87bcd0bef64081a48f4574f9a4937ff
Mharia <ms5132425@gmail.com>
2025-03-31 22:18:42 -0300

    exercicio  6

---
+++ exer6/exer6.app.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Escolha um numero e descubra seu antecessor e seu sucessor ")
+
+let ant = num1 - 1
+let suc =  parseFloat (num1)  + 1
+
+alert("O antecessor do " + num1 + " e " + ant + " o sucessor e " + suc)
