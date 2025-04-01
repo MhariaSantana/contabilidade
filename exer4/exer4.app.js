2fe99cc0836d0127a5e068abccab808f5b116a0e
Mharia <ms5132425@gmail.com>
2025-03-31 21:58:45 -0300

    Exercicio 4

---
+++ exer4/exer4.app.js
@@ -0,0 +1,7 @@
+let vl1 = prompt ("Informe o primeiro valor ")
+let vl2 = prompt("Informe o segundo valor ")
+let vl3 = prompt("Informe o terceiro valor")
+
+let resultado = vl1+ vl2+ vl3 / 3
+
+alert("A media dos três numero e igual a, " + resultado )
