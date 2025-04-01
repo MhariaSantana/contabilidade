21405b1c3c84635505fd6f51c25ec3dad3a7cd6b
Mharia <ms5132425@gmail.com>
2025-03-31 21:32:59 -0300

    exercicio 3

---
+++ exer-3/exer.app.js
@@ -0,0 +1,12 @@
+let nome = prompt ("Digite o seu nome ")
+let idade = prompt("Digite a sua idade")
+
+
+
+if(idade < 18 ){
+    alert("Ola," + nome + ", você ainda não pode fazer aula de direção")
+}
+else if(idade >= 18){
+
+    alert("Olá," + nome + ", seja bem-vindo a nossa escola ! Já fez sua matricula ?")
+}
