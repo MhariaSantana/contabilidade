cc61cfbe518e5098834a5950507817d0a75d28f1
Mharia <ms5132425@gmail.com>
2025-03-31 22:05:49 -0300

    Exercicio 5

---
+++ exer5/exer5.app.js
@@ -0,0 +1,5 @@
+let temp = prompt ("Digite a temperatura em celsius para ser convertida em fahrenheit")
+
+let resultado = 1.8* temp + 32
+
+alert("A temperatura e igual a," + resultado + "fahrenheit")
