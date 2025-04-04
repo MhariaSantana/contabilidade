537bd952bbf974c965262aea975d321168da5f76
Mharia <ms5132425@gmail.com>
2025-04-04 19:44:27 -0300

    atividade teste

--- exer4/exer4.app.js
+++ exer4/exer4.app.js
@@ -1,7 +1,8 @@
 let vl1 = prompt ("Informe o primeiro valor ")
 let vl2 = prompt("Informe o segundo valor ")
 let vl3 = prompt("Informe o terceiro valor")
+let vl4 = prompt("Informe a quarta nota")
 
-let resultado = vl1+ vl2+ vl3 / 3
+let resultado = vl1+ vl2+ vl3 + vl4 / 3
 
 alert("A media dos três numero e igual a, " + resultado )
---
+++ exercicio-atrasado/atrasado.app.js
@@ -0,0 +1,18 @@
+let vl1 = prompt("Informe o seu peso ")
+let vl2 = prompt("Informe a sua altura ")
+
+let resultado = vl2 * vl2 / vl1
+
+if (resultado == 40){
+    let resultado = vl2 * vl2 / vl1
+    alert ("Seu imc e " + resultado + " você esta obesa")
+}
+else if (resultado == 16){
+    let resultado = vl2 * vl2 / vl1
+    alert("Seu IMC e " + resultado + " você está abaixo do peso ")
+}
+else if (resultado == 18){
+    let resultado = vl2 * vl2 / vl1
+    alert("Seu IMC e " + resultado + " seu peso está adequado ")
+}
+ 
