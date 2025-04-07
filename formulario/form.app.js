2da73a224063a86bb193010627c863516b1ed0eb
Mharia <ms5132425@gmail.com>
2025-04-07 20:19:22 -0300

    formularios

---
+++ formulario/form.app.js
@@ -0,0 +1,10 @@
+function fnSomar(){
+    let n1 = document.getElementById("n1").value
+    let n2 = document.getElementById("n2").value
+    let total = parseFloat(n1) + parseFloat(n2)
+    console.log(total)
+    document.getElementById("resultado").value = total 
+    document.getElementById("outro-reultado".innerText = total1)
+    
+
+}
