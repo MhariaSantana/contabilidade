e89fdfa40819468b4b3ee847051141319bec8cdc
Mharia <ms5132425@gmail.com>
2025-04-07 21:26:44 -0300

    formulario

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function fnMostrarHoraAtual(){
     let dataHoraCompleta = new Date()
     let horas = dataHoraCompleta.getHours()
     let minutos = dataHoraCompleta.getMinutes()
-    let horaAtual = horas +
+    let horaAtual = horas + minutos 
 
 }
 
@@ -48,3 +48,14 @@ function fnTrocarTitulo(){
     document.getElementById("titulo").innerText= "Novo titulo"
     
 }
+
+function fnRegistrarContato(){
+    let nome = document.getElementsByName("nome").value
+    let email = document.getElementById("email").value
+    let mensagem = document.getElementById("mensagem").value
+    let informacoesCompleta = nome + " " +  email + " <br>"+ mensagem
+    document.getElementById("informacoes").innerHTML = informacoesCompleta
+}
+document.getElementById("nome").value = " "
+document.getElementById("email").value = " "
+document.getElementById("mensagem").value = " "
