d6d094b474e8819a22974b2747667296e0265d08
Mharia <ms5132425@gmail.com>
2025-04-09 21:15:44 -0300

    form

---
+++ exer-8/exerc8.app.js
@@ -0,0 +1,9 @@
+let vl1 = prompt ("Informe o primeiro valor ")
+let vl2 = prompt("Informe o segundo valor ")
+let vl3 = prompt("Informe o terceiro valor")
+let vl4 = prompt("Informe o quarto valor")
+let vl5 = prompt("Informe o quinto valor ")
+
+let resultado = vl1+ vl2+ vl3 + vl4 + vl5/ 3
+
+alert("A media dos três numero e igual a, " + resultado )
